7b363bca0bb3938f57475daad45fee3f4e919abb
xy <xiaoyi@tanyue.cc>
2025-04-14 18:42:57 +0800

    feat: 注释测试的指令执行

--- index.js
+++ index.js
@@ -18,37 +18,37 @@ export const pugSiteCore = {
   translateLanguageData,
 };
 
-let curCmd = process.env.npm_lifecycle_event;
+// let curCmd = process.env.npm_lifecycle_event;
 
-try {
-  switch (curCmd) {
-    case "getFun":
-      await generateGetDataFn();
-      break;
-    case "getData":
-      await generateGetDataFn();
-      const args = process.argv.slice(2);
-      await fetchDataToJsonFile(args);
-      break;
-    case "dev":
-      startDevServer();
-      break;
-    case "compileFn":
-      await compilePagesPugToFn();
-      break;
-    case "buildFn":
-      await buildFn();
-      break;
-    case "buildStatic":
-      await buildStatic();
-      break;
-    case "lang":
-      await translateLanguageData();
-      break;
-    default:
-      console.log(`未知的命令: ${curCmd}`);
-  }
-} catch (error) {
-  console.error(`执行命令 ${curCmd} 时发生错误:`, error);
-  process.exit(1);
-}
+// try {
+//   switch (curCmd) {
+//     case "getFun":
+//       await generateGetDataFn();
+//       break;
+//     case "getData":
+//       await generateGetDataFn();
+//       const args = process.argv.slice(2);
+//       await fetchDataToJsonFile(args);
+//       break;
+//     case "dev":
+//       startDevServer();
+//       break;
+//     case "compileFn":
+//       await compilePagesPugToFn();
+//       break;
+//     case "buildFn":
+//       await buildFn();
+//       break;
+//     case "buildStatic":
+//       await buildStatic();
+//       break;
+//     case "lang":
+//       await translateLanguageData();
+//       break;
+//     default:
+//       console.log(`未知的命令: ${curCmd}`);
+//   }
+// } catch (error) {
+//   console.error(`执行命令 ${curCmd} 时发生错误:`, error);
+//   process.exit(1);
+// }
