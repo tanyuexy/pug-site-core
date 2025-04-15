08b7a0b5ef91b491dbc6e0aabea75ce34e7f47e8
xy <xiaoyi@tanyue.cc>
2025-04-15 18:28:52 +0800

    feat: 优化并发处理现在可以精细到一个数据请求

--- index.js
+++ index.js
@@ -18,37 +18,44 @@ export const pugSiteCore = {
   translateLanguageData,
 };
 
-// let curCmd = process.env.npm_lifecycle_event;
+let curCmd = process.env.npm_lifecycle_event;
+const args = process.argv.slice(2);
 
-// try {
-//   switch (curCmd) {
-//     case "getFun":
-//       await generateGetDataFn();
-//       break;
-//     case "getData":
-//       await generateGetDataFn();
-//       const args = process.argv.slice(2);
-//       await fetchDataToJsonFile(args);
-//       break;
-//     case "dev":
-//       startDevServer();
-//       break;
-//     case "compileFn":
-//       await compilePagesPugToFn();
-//       break;
-//     case "buildFn":
-//       await buildFn();
-//       break;
-//     case "buildStatic":
-//       await buildStatic();
-//       break;
-//     case "lang":
-//       await translateLanguageData();
-//       break;
-//     default:
-//       console.log(`未知的命令: ${curCmd}`);
-//   }
-// } catch (error) {
-//   console.error(`执行命令 ${curCmd} 时发生错误:`, error);
-//   process.exit(1);
-// }
+// 检查是否包含 dev 参数
+if (args.includes("dev")) {
+  try {
+    switch (curCmd) {
+      case "getFun":
+        await generateGetDataFn();
+        break;
+      case "getData":
+        await generateGetDataFn();
+        // 移除 dev 参数后传递其他参数
+        const dataArgs = args.filter((arg) => arg !== "dev");
+        await fetchDataToJsonFile(dataArgs);
+        break;
+      case "dev":
+        startDevServer();
+        break;
+      case "compileFn":
+        await compilePagesPugToFn();
+        break;
+      case "buildFn":
+        await buildFn();
+        break;
+      case "buildStatic":
+        await buildStatic();
+        break;
+      case "lang":
+        await translateLanguageData();
+        break;
+      default:
+        console.log(`未知的命令: ${curCmd}`);
+    }
+  } catch (error) {
+    console.error(`执行命令 ${curCmd} 时发生错误:`, error);
+    process.exit(1);
+  }
+} else {
+  console.log(`请添加 dev 参数以执行命令，例如: npm run getData dev`);
+}
